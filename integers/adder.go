e3e2d80255310f15037492ad1bf124c106d67317
jian.li <jian.li@shopeemobile.com>
2020-04-13 13:17:19 +0800

    update iteration

--- integers/adder.go
+++ integers/adder.go
@@ -4,12 +4,12 @@ import "fmt"
 
 // Add takes two integers and returns the sum of them
 func Add(a, b int) int {
-    sum := a + b
-    return sum
+	sum := a + b
+	return sum
 }
 
 func ExampleAdd() {
-    sum := Add(1, 5)
-    fmt.Println(sum)
-    // Output: 6
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,11 +1,19 @@
 package iteration
 
-// import "fmt"
+import "fmt"
 
+// Repeat a character 5 times
 func Repeat(character string) string {
 	var repeated string
-    for i := 0; i < 5; i++ {
-        repeated += character
-    }
-    return repeated
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return repeated
+}
+
+// ExampleRepeat
+func ExampleRepeat() {
+	result := Repeat("a")
+	fmt.Println(result)
+	// Output: aaaaa
 }
