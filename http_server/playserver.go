f74658022e6f59378df82ae283ce8dd1056c41b1
l33klin <l33klin@gmail.com>
2020-05-11 01:14:00 +0800

    add http_server

---
+++ http_server/playserver.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+}
